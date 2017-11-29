1aa2ed8365394e263863daef640ee3760fcce097
DESKTOP-KTC9KRM\K <pgopose2>
2017-11-29 16:32:34 +0900

    수정 -효정

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -229,28 +229,6 @@ $(document).ready(function () {
         return false;
     });
 
-    /* 게시판 이미지 가운데 맞춰자르기 */
-
-    $(".board_data img").show(function () {
-        var div = $(".board_data"); // 이미지를 감싸는 div
-        var img = // 이미지
-        var divAspect = 3 / 2; // div의 가로세로비는 알고 있는 값이다
-        var imgAspect = img.height / img.width;
-
-        if (imgAspect <= divAspect) {
-            // 이미지가 div보다 납작한 경우 세로를 div에 맞추고 가로는 잘라낸다
-            var imgWidthActual = div.offsetHeight / imgAspect;
-            var imgWidthToBe = div.offsetHeight / divAspect;
-            var marginLeft = -Math.round((imgWidthActual - imgWidthToBe) / 2);
-            img.style.cssText = 'width: auto; height: 100%; margin-left: ' +
-                marginLeft + 'px;'
-        } else {
-            // 이미지가 div보다 길쭉한 경우 가로를 div에 맞추고 세로를 잘라낸다
-            img.style.cssText = 'width: 100%; height: auto; margin-left: 0;';
-        }
-    });
-
-
 
     /* 후원 email form */
     $('.email_select').change(function () {
